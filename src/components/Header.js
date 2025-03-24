62f8c132a7304a048046232c152ab18f9ce8b1d9
Munazahqa <munazahmehdi@gmail.com>
2025-03-24 17:16:17 +0500

    favicon changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header() {
     return (
-      <header className="bg-primary text-white p-4 w-100 shadow-sm border-bottom">
+      <header className="bg-primary text-white p-4 w-200 shadow-sm border-bottom">
         <h2 className="text-center m-0">Proactive Autoscaler System</h2>
       </header>
     );
